9ca48784b4fcf32fa9c707a23239816907fb2ef2
zahangir <alamzahangir218@gmail.com>
2025-04-27 14:04:32 +0600

    some funtion is added

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
-
 document.getElementById('btn-donate-1').addEventListener('click', function(event){
-event.preventDefault();
-// console.log('btn-donate-1')
-
-const inputNumber = document.getElementById('input-number').value;
-const numberParseFloar = parseFloat(inputNumber)
-// console.log(numberParseFloar);
-const balanceInput1 = document.getElementById('balance-1').innerText;
-const balanceParseFloar = parseFloat(balanceInput1);
-// console.log(balanceParseFloar)
-
-const sumTotal = balanceParseFloar + numberParseFloar;
-// console.log(sumTotal);
-
-document.getElementById('balance-1').innerText=sumTotal;
-document.getElementById('balanceParseFloar').innerText=newBalance;
-
-})
-// document.getElementById('balanceParseFloar').innerText=newBalance;
-
+    event.preventDefault();
+    
+    const inputNumber = document.getElementById('input-number').value;
+    const numberParseFloat = parseFloat(inputNumber);
+    
+    const balanceInput1 = document.getElementById('balance-1').innerText;
+    const balanceParseFloat = parseFloat(balanceInput1);
+    
+    const sumTotal = balanceParseFloat + numberParseFloat;
+    
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const mainBalanceFloat = parseFloat(mainBalance);
+    console.log(mainBalanceFloat);
+    
+    const reduceBalance = mainBalanceFloat + numberParseFloat;
+    console.log(reduceBalance);
+    
+    document.getElementById('balance-1').innerText = sumTotal;
+    document.getElementById('main-balance').innerText = reduceBalance;
+    document.getElementById('input-number').value = ' ';
+});
 
 // second card
 
@@ -30,76 +31,68 @@ document.getElementById('btn-donate-2').addEventListener('click', function(event
     const inputNumber2 = document.getElementById('input-number-2').value;
     const numberParseFloar = parseFloat(inputNumber2)
     console.log(numberParseFloar);
+
     const balanceInput2 = document.getElementById('balance-2').innerText;
     const balanceParseFloar = parseFloat(balanceInput2);
     console.log(balanceParseFloar)
     
     const sumTotal = balanceParseFloar + numberParseFloar;
     console.log(sumTotal);
+
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const mainBalance2Float = parseFloat(mainBalance)
+    console.log(mainBalance2Float);
+    
+    const reduceBalance =  mainBalance2Float + numberParseFloar + balanceParseFloar;
+    console.log(reduceBalance);
     
     document.getElementById('balance-2').innerText=sumTotal;
-    document.getElementById('balanceParseFloar').innerText=newBalance;
+    document.getElementById('main-balance').innerText=reduceBalance;
+    document.getElementById('input-number-2').value=' ';
     
     })
-    // document.getElementById('balanceParseFloar').innerText=newBalance;
+
+   
     
 
     // thirt card
+    // (isNaN(balanceParseFloat) ? 2400 : 0);
 
-    document.getElementById('btn-donate-3').addEventListener('click', function(event){
-        event.preventDefault();
-        console.log('btn-donate-3')
+
+    // document.getElementById('btn-donate-3').addEventListener('click', function(event){
+    //     event.preventDefault();
+    //     console.log('btn-donate-3')
         
-        const inputNumber3 = document.getElementById('input-number-3').value;
-        const numberParseFloar = parseFloat(inputNumber3)
-        console.log(numberParseFloar);
-        const balanceInput3 = document.getElementById('balance-3').innerText;
-        const balanceParseFloar = parseFloat(balanceInput3);
-        console.log(balanceParseFloar)
+    //     const inputNumber3 = document.getElementById('input-number-3').value;
+    //     const numberParseFloar = parseFloat(inputNumber3)
+    //     console.log(numberParseFloar);
+    
+    //     const balanceInput3 = document.getElementById('balance-3').innerText;
+    //     const balanceParseFloar = parseFloat(balanceInput3);
+    //     if(balanceParseFloar === '2400'){
+    //         return balanceParseFloar;
+    //     }
+    //     const sumTotal = balanceParseFloar + numberParseFloar;
+    //     console.log(sumTotal);
+    
+    //     const mainBalance = document.getElementById('main-balance').innerText;
+    //     const mainBalance3Float = parseFloat(mainBalance)
+    //     console.log(mainBalance3Float);
         
-        const sumTotal = balanceParseFloar + numberParseFloar;
-        console.log(sumTotal);
+    //     const reduceBalance =  mainBalance3Float + numberParseFloar + (isNaN(balanceParseFloar) ? 2400 : 0);
+    //     console.log(reduceBalance);
         
-        document.getElementById('balance-3').innerText=sumTotal;
-        document.getElementById('balanceParseFloar').innerText=newBalance;
+    //     document.getElementById('balance-3').innerText=sumTotal;
+    //     document.getElementById('main-balance').innerText=reduceBalance;
+    //     document.getElementById('input-number-3').value=' ';
+        
+    //     })
        
-
-        })
+   
+    
         
-        // document.getElementById('balanceParseFloar').innerText=newBalance;
+        
         
     
 
 
-
-// document.getElementById('btn-add-money').addEventListener('click', function(event){
-//     event.preventDefault();
-   
-//     const addMoneyInput = document.getElementById('input-add-money').value;
-//     const addMoneyPin = document.getElementById('input-add-pin').value;
-//     console.log(addMoneyInput, addMoneyPin);
-  
-//     // pin verify
-  
-//     if(addMoneyPin === '1234'){
-//       console.log('money is add your account')
-  
-//       const balance = document.getElementById('total-balance').innerText;
-//       console.log(balance)
-  
-//       const addMoneyParseFloat = parseFloat(addMoneyInput);
-//       const addBalanceParseFloat =parseFloat(balance);
-//       const newTotalMoney = addMoneyParseFloat + addBalanceParseFloat;
-//       console.log(newTotalMoney)
-  
-//       // update total
-//       document.getElementById('total-balance').innerText=newTotalMoney;
-//       }
-//     else{
-//       alert('failed to your add money! Please try to again');
-//     }
-//   })
-  
-//   // //  6 account update
-//   document.getElementById('balance').innerText=newBalance;
-  
