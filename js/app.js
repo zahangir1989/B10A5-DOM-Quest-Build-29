062abb95a8ea77332810df0e78c77f257d9b541c
zahangir <alamzahangir218@gmail.com>
2025-05-01 11:22:14 +0600

    clicke donate and add summary

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const donet = document.getElementById("donte-btn")
 
 historyBtn.addEventListener("click", function(){
     historyBtn.classList.add("bg-green-200")
-    historyBtn.classList.remove("bg-gray-700")
+    historyBtn.classList.remove("bg-gray-200")
     
     hiddenDev.classList.add("hidden")
     visible.classList.remove("hidden")
@@ -15,10 +15,64 @@ historyBtn.addEventListener("click", function(){
 })
 
 donet.addEventListener("click",function(){
-    donet.classList.remove("bg-gray-700")
-    donet.classList.add("bg-red-700")
+    donet.classList.remove("bg-gray-200")
+    donet.classList.add("bg-red-200");
     
    
     hiddenDev.classList.remove("hidden")
     visible.classList.add("hidden")
+})
+
+
+// document.getElementById('btn-donate-1').addEventListener('click', function(){
+//     // console.log("btn is clicked now")
+//     const sumary = document.getElementById("headline");
+//     const newSumary = sumary.innerText;
+//     console.log(newSumary);
+
+//     const description = document.getElementById('description');
+//     const newDescription = description.innerText;
+//     console.log(newDescription);
+
+//     const visibleContainer = document.getElementById('visible');
+//     const p = document.createElement('p');
+//     p.innerText= newSumary;
+//     p.innerText= newDescription
+//     visibleContainer.appendChild(p);
+// })
+
+document.getElementById('btn-donate-1').addEventListener('click', function(){
+ 
+    const fullDivInner = document.getElementById('fullDiv1');
+    const displayDiv = fullDivInner.innerText;
+    console.log(displayDiv);
+
+const visibleContainer = document.getElementById('visible');
+const  p = document.createElement('p');
+p.innerText = displayDiv;
+visibleContainer.appendChild(p);
+})
+
+document.getElementById('btn-donate-2').addEventListener('click', function(){
+ 
+    const fullDivInner = document.getElementById('fullDiv2');
+    const displayDiv = fullDivInner.innerText;
+    console.log(displayDiv);
+
+const visibleContainer = document.getElementById('visible');
+const  p = document.createElement('p');
+p.innerText = displayDiv;
+visibleContainer.appendChild(p);
+})
+
+document.getElementById('btn-donate-3').addEventListener('click', function(){
+ 
+    const fullDivInner = document.getElementById('fullDiv1');
+    const displayDiv = fullDivInner.innerText;
+    console.log(displayDiv);
+
+const visibleContainer = document.getElementById('visible');
+const  p = document.createElement('p');
+p.innerText = displayDiv;
+visibleContainer.appendChild(p);
 })
